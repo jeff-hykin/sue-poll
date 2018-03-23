46e128a5356f6ff520853497e5450f1a674b7aee
Jeff Hykin <jeff.hykin@gmail.com>
2018-03-22 22:17:18 -0500

    Update sue_poll.py

--- sue_poll.py
+++ sue_poll.py
@@ -1,60 +1,64 @@
-
-
-poll = {
+poll_data = {
     "letter_options" : [],
-    "poll["options"]" : []
+    "poll_data["options"]" : []
     "question" : "",
     "poll":{}
 }
-
-def poll(textBody):
+@bp.route('/poll')
+def poll():
     """!poll <name/question>\n-<option1>  \n-<option2> ... \n<option n>"""
-    global poll
+    global poll_data
     
+    textBody = msg.textBody
+    sender = msg.buddyId
+
     options = textBody.split('\n-') # FIXME, not sure if options would include the 'poll' or '!poll' 
     
     # get the poll question 
-    poll["question"] = options.pop(0)
+    poll_data["question"] = options.pop(0)
     # get the poll options 
-    poll["options"] = options 
+    poll_data["options"] = options 
     # reset the last poll and replace it with a new poll 
-    poll["letter_options"] = []
-    poll["vote_tracker"] = {}
+    poll_data["letter_options"] = []
+    poll_data["vote_tracker"] = {}
     # create the output to display 
-    output_ = poll["question"] + "\n"
-    # regenerate variables that will display the poll["options"] 
+    output_ = poll_data["question"] + "\n"
+    # regenerate variables that will display the poll_data["options"] 
     index_ = 64 # start right before the ASCII 'A' 
-    for each_option in poll["options"]:
+    for each_option in poll_data["options"]:
         index_ += 1
         option_letter = char(index_) 
-        poll["letter_options"].append(option_letter) 
-        poll["vote_tracker"][option_letter] = set()
+        poll_data["letter_options"].append(option_letter) 
+        poll_data["vote_tracker"][option_letter] = set()
         output_ += "\n"+option_letter+". "+each_option
 
-    rprint(output_)
+    return output_
 
-    
-        
-def vote(textBody,sender):
+@bp.route('/vote')
+def vote():
     """!vote <letter>"""
-    global poll
-    options = textBody.split(' ')  # FIXME, not sure if poll["options"] would include the 'vote' or '!vote' 
+    global poll_data
+
+    textBody = msg.textBody
+    sender = msg.buddyId
+
+    options = textBody.split(' ')  # FIXME, not sure if poll_data["options"] would include the 'vote' or '!vote' 
     
     # if there is actually a correct input
     if len(options) == 1:
         the_letter = options[0].upper()
-        # if the letter is one of the poll["options"]
-        if the_letter in poll["letter_options"]:
+        # if the letter is one of the poll_data["options"]
+        if the_letter in poll_data["letter_options"]:
             # then add the sender to that vote
-            poll["vote_tracker"][the_letter].add(sender)
+            poll_data["vote_tracker"][the_letter].add(sender)
     
     # display the new status
-    output_ = poll["question"] + "\n"
+    output_ = poll_data["question"] + "\n"
     index_ = 64 # start right before the ASCII 'A' 
-    for each_option in poll["options"]:
+    for each_option in poll_data["options"]:
         index_ += 1
         option_letter = char(index_) 
-        number_of_votes = len(poll["vote_tracker"][option_letter])
+        number_of_votes = len(poll_data["vote_tracker"][option_letter])
         output_ += "\n("+ str(number_of_votes) +" votes) "+option_letter+". "+each_option
 
-    rprint(output_)
+    return output_
